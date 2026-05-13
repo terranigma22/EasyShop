3f3ed0bd5ffdccf7543ffd56b666cb7b4dbacac8
Alexis Amaro Placeres <alexisamaroplaceres@gmail.com>
2026-05-13 08:30:06 -0400

    Add project files.

---
+++ EasyShop/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace EasyShop
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
